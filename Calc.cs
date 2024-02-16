840f46714d30c8cacbf9976d1de55c90a7074cd3
sureshreddyrayapati <resuresh0@gmail.com>
2024-02-16 10:13:41 +0530

    CalculaterRepostry

---
+++ Calc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleProjectFeb16
+{
+    public class Calc
+    {
+        public void Add(double x, double y)
+        {
+            Console.WriteLine("Result after Addition: "+(x+y));
+        }
+        public void Sub(double x, double y) { Console.WriteLine("Result aftre Sub :" + (x - y)); }
+        public void Div(double x, double y) { Console.WriteLine("Result after division :"+(x/y)); }
+        public void Mul(double x, double y)
+        {
+            Console.WriteLine("Result after Multiplication" + (x * y));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleProjectFeb16; 
+Calc cal= new Calc();
+Console.WriteLine("add calc");
+cal.Add(147 , 963);
